0e75dd601f8c4a098d216043b52ff8888b0413ac
Ryan Lee <12857462+WaveRyan@users.noreply.github.com>
2023-07-25 20:25:55 +0900

    scroll fix

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -55,8 +55,8 @@ jQuery(document).ready(function($){
 
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
+            scrollTop: target.offset().top - 80
+          }, 50);
         }
       }
     });
@@ -80,32 +80,32 @@ jQuery(document).ready(function($){
     }, 250);
 
     function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
-                $('.nav-up, .nav-down').css('top', '0px');             
-            }
-        }
-
-        lastScrollTop = st;
+    //     var st = $(this).scrollTop();
+    //
+    //     // Make sure they scroll more than delta
+    //     if(Math.abs(lastScrollTop - st) <= delta)
+    //         return;
+    //
+    //     // If they scrolled down and are past the navbar, add class .nav-up.
+    //     // This is necessary so you never see what is "behind" the navbar.
+    //     if (st > lastScrollTop && st > navbarHeight){
+    //         // Scroll Down
+    //         // $('nav').removeClass('nav-down').addClass('nav-up');
+    //         // $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
+    //
+    //     } else {
+    //         // Scroll Up
+    //         // if(st + $(window).height() < $(document).height()) {
+    //         //     $('nav').removeClass('nav-up').addClass('nav-down');
+    //         //     $('.nav-up, .nav-down').css('top', '0px');
+    //         // }
+    //     }
+    //
+    //     lastScrollTop = st;
     }
-        
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');  
-    
+
+    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
+
     // spoilers
      $(document).on('click', '.spoiler', function() {
         $(this).removeClass('spoiler');
